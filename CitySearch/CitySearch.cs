e15c6e88602c910ed2954154530acad55d48869f
Jun Hui Chia <jefferychia2014@gmail.com>
2026-03-31 19:04:00 +0100

    Set up project

---
+++ CitySearch/CitySearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CitySearch
+{
+    public class CityFinder : ICityFinder
+    {
+        public ICityResult Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CitySearch/ICityResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CitySearch
+{
+    public interface ICityResult
+    {
+        ICollection<string> NextLetters { get; set; }
+        ICollection<string> NextCities { get; set; }
+    }
+}
